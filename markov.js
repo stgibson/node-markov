36550ea2738abf6fc9ebcd7918755641e5b48a14
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-13 19:14:30 -0800

    Implemented to methods in markov.js. Implemented makeChains() and makeText(numWords=100) in MarkovMachine in markov.js.

--- markov.js
+++ markov.js
@@ -8,6 +8,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
+    this.chains = {};
     this.makeChains();
   }
 
@@ -17,13 +18,51 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    // don't need to check last word, since no words will follow it
+    for (let i = 0; i < this.words.length - 1; i++) {
+      const word = this.words[i];
+      const nextWord = this.words[i + 1];
+      if (this.chains[word] && this.chains[word].indexOf(nextWord) === -1) {
+        this.chains[word].push(nextWord);
+      }
+      else {
+        this.chains[word] = [nextWord];
+      }
+    }
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    // pick a random starting word
+    let currWord = this.words[Math.floor(Math.random() * this.words.length)];
+    let text = "";
+    let numOfWordsUsed = 0;
+    while (numOfWordsUsed < numWords) {
+      // first word isn't preceded by space
+      if (numOfWordsUsed === 0) {
+        text += currWord;
+      }
+      else {
+        text += " ";
+        text += currWord;
+      }
+      numOfWordsUsed++;
+
+      // pick next random word that could follow currWord
+      const nextWords = this.chains[currWord];
+      // if no words can follow, end text
+      if (nextWords) {
+        currWord = nextWords[Math.floor(Math.random() * nextWords.length)];
+      }
+      else {
+        break;
+      }
+    }
+
+    return text;
   }
 }
+
+module.exports = { MarkovMachine };
