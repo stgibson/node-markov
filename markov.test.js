06dde8cf2517f29055fb81615a075ab7ad66b1b3
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-14 16:44:06 -0800

    Added tests for makeText(numWords=100). Added tests in markov.test.js for makeTest(numWords=100) in markov.js.

--- markov.test.js
+++ markov.test.js
@@ -10,21 +10,21 @@ describe("makeChains() tests (with setup)", () => {
   });
 
   test("creates chain correctly when no words", () => {
-    let words = [];
+    const words = [];
     markovMachine.words = words;
     markovMachine.makeChains();
     expect(markovMachine.chains).toEqual({ });
   });
 
   test("creates chain correctly when only 1 words", () => {
-    let words = ["the"];
+    const words = ["the"];
     markovMachine.words = words;
     markovMachine.makeChains();
     expect(markovMachine.chains).toEqual({ the: [null] });
   });
 
   test("creates chain correctly for multiple words", () => {
-    let words = ["the", "cat", "in", "the", "hat"];
+    const words = ["the", "cat", "in", "the", "hat"];
     markovMachine.words = words;
     markovMachine.makeChains();
     expect(markovMachine.chains).toEqual({
@@ -36,7 +36,7 @@ describe("makeChains() tests (with setup)", () => {
   });
 
   test("adds null to word already used", () => {
-    let words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
+    const words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
     markovMachine.words = words;
     markovMachine.makeChains();
     expect(markovMachine.chains).toEqual({
@@ -46,5 +46,88 @@ describe("makeChains() tests (with setup)", () => {
       hat: ["is", null],
       is: ["in"]
     })
+  });
+});
+
+describe("makeText(numWords=100) tests (with setup)", () => {
+  beforeAll(() => {
+    markovMachine = new MarkovMachine("");
+  });
+
+  test("returns empty string when 0 words", () => {
+    const words = [];
+    const chains = {};
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    expect(markovMachine.makeText()).toEqual("");
+  });
+
+  test("returns word when 1 word", () => {
+    const words = ["the"];
+    const chains = { the: [null] };
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    expect(markovMachine.makeText()).toEqual("the");
+  });
+
+  test("returns text with multiple words", () => {
+    const words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
+    const chains = {
+      the: ["cat", "hat", "hat"],
+      cat: ["in"],
+      in: ["the", "the"],
+      hat: ["is", null],
+      is: ["in"]
+    };
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    expect(markovMachine.makeText()).not.toEqual("");
+  });
+
+  test("returns text with at most 100 words when no args passed", () => {
+    const words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
+    const chains = {
+      the: ["cat", "hat", "hat"],
+      cat: ["in"],
+      in: ["the", "the"],
+      hat: ["is"],
+      is: ["in"]
+    };
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    const text = markovMachine.makeText();
+    const textWords = text.split(" ");
+    expect(textWords.length).toEqual(100);
+  });
+
+  test("returns empty string when 0 passed", () => {
+    const words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
+    const chains = {
+      the: ["cat", "hat", "hat"],
+      cat: ["in"],
+      in: ["the", "the"],
+      hat: ["is", null],
+      is: ["in"]
+    };
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    expect(markovMachine.makeText(numWords=0)).toEqual("");
+  });
+
+  test("returns text with number of words restricted by number passed", () => {
+    const words = ["the", "cat", "in", "the", "hat", "is", "in", "the", "hat"];
+    const chains = {
+      the: ["cat", "hat", "hat"],
+      cat: ["in"],
+      in: ["the", "the"],
+      hat: ["is"],
+      is: ["in"]
+    };
+    markovMachine.words = words;
+    markovMachine.chains = chains;
+    const limit = 50;
+    const text = markovMachine.makeText(limit);
+    const textWords = text.split(" ");
+    expect(textWords.length).toEqual(limit);
   });
 });
