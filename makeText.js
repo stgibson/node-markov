f52782b3a7adff501a9e406eb15c7938e9d92e89
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-14 17:31:58 -0800

    Installed axios and added code to makeText.js. Installing axios modified package.json and package-lock.json.

--- makeText.js
+++ makeText.js
@@ -1,2 +1,47 @@
-/** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const axios = require("axios");
+const { MarkovMachine } = require("./markov");
 
+const errorMessage =
+    'Usage: node makeText.js <type> <address>\n' +
+    '<type> is either "file" or "url"' + 
+    ' and <address> is the location of the file or webpage';
+
+async function main() {
+  // first check if valid number of arguments
+  if (process.argv.length !== 4) {
+    console.error(errorMessage);
+    process.exit(1);
+  }
+
+  const type = process.argv[2];
+  if (type === "file") {
+    const path = process.argv[3];
+    fs.readFile(path, "utf8", (errors, data) => {
+      if (errors) {
+        console.dir(errors);
+        process.exit(1);
+      }
+      const markovMachine = new MarkovMachine(data);
+      console.log(markovMachine.makeText());
+    });
+  }
+  else if (type === "url") {
+    const url = process.argv[3];
+    try {
+      const resp = await axios.get(url);
+      const markovMachine = new MarkovMachine(resp.data);
+      console.log(markovMachine.makeText());
+    }
+    catch(error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+  else {
+    console.error(errorMessage);
+    process.exit(1);
+  }
+}
+
+main();
